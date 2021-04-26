11349200e589fab10fe849269e07eb8eb83a0fcf
Dick Carrillo <dcarrillom@gmail.com>
2021-04-26 11:41:17 +0300

    updating ...

--- project_backend_uavs.py
+++ project_backend_uavs.py
@@ -643,21 +643,21 @@ def get_random_locationshexagon_neighbors(N,K,R,min_dist,equal_number_for_BS = T
 
 
     # Custom BSs location
-   # TX_loc_uavs = np.loadtxt('uav_location.txt')
-   # TX_loc_uavs = np.transpose(TX_loc_uavs)
+    TX_loc_uavs = np.loadtxt('uav_location.txt')
+    TX_loc_uavs = np.transpose(TX_loc_uavs)
 
     #if (N>0):
     for k in range(1):
-    #    TX_loc [0, generated_hexagons*1 + k] =  TX_loc_uavs[0,0] #0.0
-    #    TX_loc [1, generated_hexagons*1 + k] =  TX_loc_uavs[1,0] #0.0
-    #    TX_xhex [:,generated_hexagons*1 + k] = x_hexagon + TX_loc_uavs[0,0]
-    #    TX_yhex [:,generated_hexagons*1 + k] = y_hexagon + TX_loc_uavs[1,0]
+        TX_loc [0, generated_hexagons*1 + k] =  TX_loc_uavs[0,0] #0.0
+        TX_loc [1, generated_hexagons*1 + k] =  TX_loc_uavs[1,0] #0.0
+        TX_xhex [:,generated_hexagons*1 + k] = x_hexagon + TX_loc_uavs[0,0]
+        TX_yhex [:,generated_hexagons*1 + k] = y_hexagon + TX_loc_uavs[1,0]
 
         #ORIGINAL Code
-        TX_loc[0, generated_hexagons * 1 + k] = 0.0
-        TX_loc[1, generated_hexagons * 1 + k] = 0.0
-        TX_xhex[:, generated_hexagons * 1 + k] = x_hexagon
-        TX_yhex[:, generated_hexagons * 1 + k] = y_hexagon
+    #    TX_loc[0, generated_hexagons * 1 + k] = 0.0
+    #    TX_loc[1, generated_hexagons * 1 + k] = 0.0
+    #    TX_xhex[:, generated_hexagons * 1 + k] = x_hexagon
+    #    TX_yhex[:, generated_hexagons * 1 + k] = y_hexagon
     generated_hexagons += 1
 
     while(generated_hexagons < K):
@@ -668,8 +668,8 @@ def get_random_locationshexagon_neighbors(N,K,R,min_dist,equal_number_for_BS = T
             tmp_xloc = TX_loc [0, i]+np.sqrt(3)*R*np.cos(j*np.pi/(3))
             tmp_yloc = TX_loc [1, i]+np.sqrt(3)*R*np.sin(j*np.pi/(3))
 
-            #tmp_xloc = TX_loc_uavs[0,i]
-            #tmp_yloc = TX_loc_uavs[1,i]
+            tmp_xloc = TX_loc_uavs[0,i]
+            tmp_yloc = TX_loc_uavs[1,i]
 
 
             tmp_xhex = tmp_xloc+x_hexagon
