a260e57989ba9282ca29bc968a2c0a3fc5002a14
Dick Carrillo <dcarrillom@gmail.com>
2021-06-10 04:18:43 +0300

    updating ...

--- train_uavs_01.sh
+++ train_uavs_01.sh
@@ -20,8 +20,8 @@ wait
 
 
 echo "Train Results, each interactive Ctrl +Z to pass to the next result"
-python ./train_results.py --json-file "train_K20_N100_M4_shadow10_episode4-2500_travelIND_fd10"
+python ./train_results.py --json-file "train_K20_N100_M4_shadow10_episode4-5000_travelIND_fd10"
 #python3 -i ./train_results.py --json-file "train_K1_N20_M1_shadow10_episode4-5000_travelIND_fd10"
-python ./train_results_interference.py --json-file "train_K20_N100_M4_shadow10_episode4-2500_travelIND_fd10"
+python ./train_results_interference.py --json-file "train_K20_N100_M4_shadow10_episode4-5000_travelIND_fd10"
 #python3 -i ./train_results.py --json-file "train_K1_N20_M2_shadow10_episode4-5000_travelIND_fd10"
-python ./train_results_individualslices.py --json-file "train_K20_N100_M4_shadow10_episode4-2500_travelIND_fd10"
+python ./train_results_individualslices.py --json-file "train_K20_N100_M4_shadow10_episode4-5000_travelIND_fd10"
