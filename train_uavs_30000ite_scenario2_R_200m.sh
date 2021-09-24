e067912e68a19c69da73023da9af844791e3f230
Dick Carrillo <dcarrillom@gmail.com>
2021-09-25 02:08:25 +0300

    Adding a new script to run , it is the 4th part

---
+++ train_uavs_30000ite_scenario2_R_200m.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+mkdir fig/200m_30000iter_scenario2
+
+
+###### 200m
+
+cp uav_location_scenario2_200m.tx uav_location.txt
+
+echo "Copying the net deployment"
+echo "200m"
+cp config/deployment/train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10_r200_s2.json config/deployment/train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10.json
+
+echo "Train Slicing - Random Deployment"
+echo "200m"
+python ./random_deployment.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10" --num-sim 0 &
+wait
+
+echo "Train Slicing - Proposed Algorithm "
+echo "200m"
+python ./trainProposed.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10" --num-sim 0 &
+wait
+
+
+echo "Train - Joint Algorithm"
+echo "200m"
+python ./trainJoint.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10" --num-sim 0 &
+wait
+
+echo "Train - Get FP Benchmarks"
+echo "200m"
+python ./get_benchmarks.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10" --num-sim 0 &
+wait
+
+
+echo "Train Results, each interactive Ctrl +Z to pass to the next result"
+echo "200m"
+python ./train_results.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10"
+#python3 -i ./train_results.py --json-file "train_K1_N20_M1_shadow10_episode4-5000_travelIND_fd10"
+python ./train_results_interference.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10"
+#python3 -i ./train_results.py --json-file "train_K1_N20_M2_shadow10_episode4-5000_travelIND_fd10"
+python ./train_results_individualslices.py --json-file "train_K20_N100_M3_shadow10_episode3-10000_travelIND_fd10"
+
+cd fig
+./temscript_30000.sh 200m_30000iter_scenario2
+cd ..
+
